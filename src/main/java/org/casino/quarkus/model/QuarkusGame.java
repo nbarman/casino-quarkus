3e7cbc6426b4e600171e8044e1f5649f996d7a77
Namit Mohan Barman <barmanam@flutterint.com>
2023-05-30 11:30:52 -0400

    Updates

--- src/main/java/org/casino/quarkus/model/QuarkusGame.java
+++ src/main/java/org/casino/quarkus/model/QuarkusGame.java
@@ -3,7 +3,6 @@ package org.casino.quarkus.model;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
 
 import jakarta.enterprise.context.ApplicationScoped;
-import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
